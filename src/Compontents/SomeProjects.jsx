9694ce271d8d9ae0dabefa4e395d9f1b8030a0df
Youssef-Magdy-19 <youssefostora1155@gmail.com>
2025-09-16 19:07:15 +0300

    change from xl to lg in home page

--- src/Compontents/SomeProjects.jsx
+++ src/Compontents/SomeProjects.jsx
@@ -18,7 +18,7 @@ const SomeProjects = () => {
     return (
         <section className='someWorks container flex flex-col w-full' style={{ padding: '100px 10px 50px 10px'}}>
             <h2 className='font-semibold text-3xl md:text-4xl'>Shaping Tomorrow's Projects Together</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full gap-[1.5rem] " style={{marginTop:"30px"}}>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-[1.5rem] " style={{marginTop:"30px"}}>
                 {data.slice(0,6).map((item, index) => (
                     <CardProject key={index} img={item.img} title={item.title} desc={item.description} id={item.id} animation={itemVariants} />
                 ))}
